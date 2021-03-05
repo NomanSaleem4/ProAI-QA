2158f7cb024a78bfae724bfc4ebbb763d46e439b
Noman <nomansaleem92@gmail.com>
2021-03-06 02:21:00 +0500

    code pushed

---
+++ qa.py
@@ -0,0 +1,15 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+
+st.title('This is first app ustaad')
+st.write("Here's our first attempt at using streamlit")
+
+
+# st.write("Here's our first attempt at using data to create a table:")
+# st.write(pd.DataFrame({
+#     'first column': [1, 2, 3, 4],
+#     'second column': [10, 20, 30, 40]
+# }))
